3886ac8dfa15af59ee59a7f0a5c083f6a5ceca9f
Minin <70429604+MininxD@users.noreply.github.com>
2024-02-07 00:49:20 +0700

    Add files via upload

---
+++ terabox.js
@@ -0,0 +1,63 @@
+import {teraApi} from './js/apiList.js';
+
+export function fetchTerabox() {
+let teraLink = inputEl.value;
+
+loadingEl.style.display = "block";
+errorMsg.style.display ="none";
+
+fetch(teraApi + teraLink).then((res) => {
+  return res.json();
+}).then((data) => {
+  downloadBtn.style.display ="block";
+  loadingEl.style.display = "none";
+  downloadLinkEl.setAttribute("href", data.linkdl[0].link);
+  linkTextEl.append(data.linkdl[0].link);
+  
+  
+  
+  
+  if(data.error) {
+    downloadBtn.style.display ="none";
+    errorMsg.style.display ="block";
+    errorMsg.classList.add("is-warning")
+    errorText.innerHTML = "Error: Link masih kosong";
+    loadingEl.style.display = "none";
+  }
+  
+  if(data.linkdl == "") {
+    downloadBtn.style.display ="none";
+    errorMsg.style.display ="block";
+  errorMsg.classList.add("is-warning")
+  errorText.innerHTML = "Error: Masukan link dengan benar";
+  loadingEl.style.display = "none";
+  }
+  
+  
+}).catch((e) => {
+  
+console.log(e.message);
+
+if (e.message.includes("reading '0'")) {
+  errorMsg.style.display ="block";
+    errorMsg.classList.add("is-warning")
+    errorText.innerHTML = "Error: Link masih kosong";
+    loadingEl.style.display = "none";
+} 
+
+else if (e.message.includes("reading 'link'")) {
+  errorMsg.style.display ="block";
+  errorMsg.classList.add("is-warning")
+  errorText.innerHTML = "Error: Masukan link dengan benar";
+  loadingEl.style.display = "none";
+  
+} else {
+    errorMsg.style.display ="block";
+  errorMsg.classList.add("is-danger")
+    errorText.innerHTML = "Error: Cek koneksi internet kamu";
+    loadingEl.style.display = "none";
+}
+})
+
+
+}
