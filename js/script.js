39fc4ba5ced7ae26ec701b700fe502215152b2f1
Minin <70429604+MininxD@users.noreply.github.com>
2024-02-07 00:50:03 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+import {fetchTerabox} from '../terabox.js';
 
+inputEl.addEventListener("focus", function() {
+  footer.style.display ="none"
+})
+inputEl.addEventListener("blur", function() {
+  footer.style.display ="block"
+})
+fetchLink.addEventListener("click", function() {
+  fetchTerabox();
+
+})
+copyLinkEl.addEventListener("click", function() {
+  navigator.clipboard.writeText(linkTextEl.textContent)
+})
+
+hideError.addEventListener("click", function() {
+  errorMsg.style.display = "none"
+})
