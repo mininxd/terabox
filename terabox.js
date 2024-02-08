112770fb227747cb1f38db81c6cac00ccc7781b4
Minin <70429604+MininxD@users.noreply.github.com>
2024-02-08 08:13:18 +0700

    Add files via upload

--- terabox.js
+++ terabox.js
@@ -3,19 +3,24 @@ import {teraApi} from './js/apiList.js';
 export function fetchTerabox() {
 let teraLink = inputEl.value;
 
+fetchLink.setAttribute("disabled", "");
+fetchLink.classList.add("disabled", "is-loading");
 loadingEl.style.display = "block";
 errorMsg.style.display ="none";
 
 fetch(teraApi + teraLink).then((res) => {
   return res.json();
 }).then((data) => {
+  fetchLink.removeAttribute("disabled", "");
+fetchLink.classList.remove("disabled", "is-loading");
   downloadBtn.style.display ="block";
   loadingEl.style.display = "none";
+  
   downloadLinkEl.setAttribute("href", data.linkdl[0].link);
   linkTextEl.append(data.linkdl[0].link);
   
   
-  
+  console.log(data)
   
   if(data.error) {
     downloadBtn.style.display ="none";
@@ -35,7 +40,9 @@ fetch(teraApi + teraLink).then((res) => {
   
   
 }).catch((e) => {
-  
+
+fetchLink.removeAttribute("disabled", "");
+fetchLink.classList.remove("disabled", "is-loading");
 console.log(e.message);
 
 if (e.message.includes("reading '0'")) {
@@ -54,7 +61,7 @@ else if (e.message.includes("reading 'link'")) {
 } else {
     errorMsg.style.display ="block";
   errorMsg.classList.add("is-danger")
-    errorText.innerHTML = "Error: 500 Internal server Error <br> - Failed To Fetch Terabox";
+    errorText.innerHTML = "Error: Cek koneksi internet kamu";
     loadingEl.style.display = "none";
 }
 })
